5729df810b304265219fea080f6f276fc38d976e
Ludmil <ludmil9595@gmail.com>
2020-04-18 15:30:00 +0300

    fully working dice app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+import 'package:flutter/services.dart';
 
 void main() {
   return runApp(
@@ -15,9 +19,47 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftButtonPressed = 6;
+  int rightButtonPressed = 6;
+
+  void randomNumber() {
+    setState(() {
+      leftButtonPressed = Random().nextInt(6) + 1;
+      rightButtonPressed = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                randomNumber();
+                print('Left is pressed');
+              },
+              child: Image.asset('images/dice$leftButtonPressed.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                randomNumber();
+                print('Right is pressed');
+              },
+              child: Image.asset('images/dice$rightButtonPressed.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
